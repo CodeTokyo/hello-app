cc0e5620072cee55f55a6f058f4adf1cc9d6df04
CodeTokyo <b-man225@hotmail.com>
2019-03-05 20:45:30 -0600

    Add routes and static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,8 @@ class StaticPagesController < ApplicationController
   #web requests arrives at hello action in static_pages controller
   #Controller Actions are just methods written inside of a controller class
   def hello
-    render 'hello'
+  end
 
+  def home    
   end
 end
